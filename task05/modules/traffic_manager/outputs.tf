022f57f4870beaa540e87590702e6ccad78857eb
aryan_pramodkumarvarma <aryan_pramodkumarvarma@epam.com>
2026-04-21 08:58:33 +0000

    Added some formting and outputs variabls

--- task05/modules/traffic_manager/outputs.tf
+++ task05/modules/traffic_manager/outputs.tf
@@ -1,5 +1,8 @@
-output "traffic_manager_fqdn" {
-  value       = azurerm_traffic_manager_profile.this.fqdn
-  description = "FQDN of the traffic manager profile"
+output "traffic_manager" {
+  value = {
+    name = azurerm_traffic_manager_profile.this.name
+    id   = azurerm_traffic_manager_profile.this.id
+    fqdn = azurerm_traffic_manager_profile.this.fqdn
+  }
 
 }
---
+++ task05/outputs.tf
@@ -0,0 +1,4 @@
+output "traffic_manager_fqdn" {
+  value = module.TRAFFIC_MANAGER.traffic_manager.fqdn
+
+}
